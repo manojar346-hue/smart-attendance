4f756db0d22f5e77342e945836c96ed0db8d0307
manojar346-hue <manojar346@gmail.com>
2026-05-01 13:54:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,31 +4,22 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 
 dotenv.config();
-
-// Connect to MongoDB
 connectDB();
 
 const app = express();
-
-// Middlewares
 app.use(cors());
-app.use(express.json({ limit: "50mb" })); // Accept images for face recognition
+app.use(express.json({ limit: "50mb" }));
 
-// Import Routes
 import authRoutes from "./routes/authRoutes.js";
 import studentRoutes from "./routes/studentRoutes.js";
 import attendanceRoutes from "./routes/attendanceRoutes.js";
 
-// Use Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/students", studentRoutes);
 app.use("/api/attendance", attendanceRoutes);
 
-// Test Route
 app.get("/", (req, res) => {
-  res.send("Smart Attendance Backend Running Successfully ✔");
+    res.send("Smart Attendance Backend Running");
 });
 
-// Start Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(5000, () => console.log("Server running on port 5000"));
